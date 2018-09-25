d09eabec3fda3fd6c90b403e2d6d55b77fb44fae
Victor Hugo Olvera Cruz <victor olvera@tiempodev.local>
2018-09-25 18:00:16 -0500

    Use of constants to make better code

--- weather-app/src/components/WeatherData.js
+++ weather-app/src/components/WeatherData.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
+import {
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WINDY,
+} from './../constants/Weathers';
 
 //Constant
 const WeatherData = () => (
     <div>
         <WeatherTemperature
             temperature={20}
-            weatherState={''}/>
+            weatherState={RAIN}/>
         <WeatherExtraInfo
             humidity={80}
             wind={"10 m/s"}/>
--- weather-app/src/components/WeatherTemperature.js
+++ weather-app/src/components/WeatherTemperature.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import WeatherIcons from 'react-weathericons';
 import WeatherExtraInfo from "./WeatherExtraInfo";
+import {
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WINDY,
+} from './../constants/Weathers';
 
 //Constant where we determinate the state and catalogue of icons
 const icons = {
-    sunny: "day-sunny",
-    fog: "day-fog",
+    [CLOUD]: "cloud",
+    [CLOUDY]: "cloudy",
+    [SUN]: "day-sunny",
+    [RAIN]: "rain",
+    [SNOW]: "snow",
+    [WINDY]: "windy",
 };
 
 //Function to get the correct weather icon
