5e65f62504a5f86c7ae77952c53fa76e0c87511f
Victor Hugo Olvera Cruz <victor olvera@tiempodev.local>
2018-09-25 17:46:09 -0500

    Use of Function in a functional component

--- weather-app/src/components/WeatherTemperature.js
+++ weather-app/src/components/WeatherTemperature.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import WeatherIcons from 'react-weathericons';
+import WeatherExtraInfo from "./WeatherExtraInfo";
 
-const WeatherTemperature = () =>(
-    <div><span>25°</span></div>
+//Constant where we determinate the state and catalogue of icons
+const icons = {
+    sunny: "day-sunny",
+    fog: "day-fog",
+};
+
+//Function to get the correct weather icon
+const getWeatherIcon = weatherState => {
+    const icon = icons[weatherState];
+    if(icon)
+        return <WeatherIcons name={icon} size="2x" />;
+    else
+        return <WeatherIcons name={"day-sunny"} size="2x" />;
+};
+
+const WeatherTemperature = ({temperature, weatherState}) =>(
+    <div>
+        {
+            getWeatherIcon(weatherState)
+        }
+        <span>{`${temperature} C°`}</span>
+    </div>
 );
 
 export default WeatherTemperature;
